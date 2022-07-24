6186b7d54b2905d88bceab462e12b36a50910b81
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-24 01:08:35 -0500

    Add Do While example.

--- Program.vb
+++ Program.vb
@@ -2,13 +2,40 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim factorial As Integer = 1
-        For i = 1 To 5
-            factorial *= i
-        Next
-        Console.WriteLine(factorial)
+        Do
+            Console.Write("Do you want to exit (Y/n): ")
+        Loop While Not Console.ReadLine().ToLower().Equals("y")
 
-        For i = 0 To 10 Step 2
+        Console.WriteLine(New String("-", 5))
+
+        Do
+            Console.Write("Do you want to exit (Y/n): ")
+        Loop Until Console.ReadLine().ToLower().Equals("y")
+
+        Console.WriteLine(New String("*", 5))
+
+        While True
+            Console.Write("Do you want to exit (Y/n): ")
+            If Console.ReadLine().ToLower().Equals("y") Then
+                Exit While
+            End If
+        End While
+
+        Console.WriteLine(New String("+", 5))
+
+        Do While True
+            Console.Write("Do you want to exit (Y/n): ")
+            If Console.ReadLine().ToLower().Equals("y") Then
+                Exit Do
+            End If
+        Loop
+
+        Console.WriteLine(New String("=", 5))
+
+        For i = 0 To 10
+            If i Mod 2 <> 0 Then
+                Continue For
+            End If
             Console.WriteLine(i)
         Next
     End Sub
