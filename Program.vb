b80e883b378b5960a20883b90467d1d32633f367
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-24 00:02:40 -0500

    Add If Else example.

--- Program.vb
+++ Program.vb
@@ -2,12 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim number As Integer = "56"
-        Console.WriteLine(number)
-        Dim str As String = CStr(number)
-        Console.WriteLine(str)
-        number = 82
-        Dim bool As Boolean = Integer.TryParse(str, number)
-        Console.WriteLine(bool)
+        Dim grade As Decimal = 59.9
+        If grade >= 60 Then
+            Console.WriteLine("You passed!")
+        ElseIf grade >= 59 AndAlso grade < 60 Then
+            Console.WriteLine("You were so close!")
+        Else
+            Console.WriteLine("You failed horribly!")
+        End If
     End Sub
 End Module
