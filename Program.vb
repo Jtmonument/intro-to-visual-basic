8cceef3dd0fa1221bbbb37c9bcf65ba73477193b
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 22:25:34 -0500

    Add Getting Input example.

--- Program.vb
+++ Program.vb
@@ -2,6 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("What is your name?")
+        Console.WriteLine("Hello " & Console.ReadLine())
     End Sub
 End Module
