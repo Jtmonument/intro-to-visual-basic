f3bda31bca1412dd7017611eebd98fbc67ce0c87
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-24 00:07:50 -0500

    Add Select Case example

--- Program.vb
+++ Program.vb
@@ -2,13 +2,18 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim grade As Decimal = 59.9
-        If grade >= 60 Then
-            Console.WriteLine("You passed!")
-        ElseIf grade >= 59 AndAlso grade < 60 Then
-            Console.WriteLine("You were so close!")
-        Else
-            Console.WriteLine("You failed horribly!")
-        End If
+        Dim finalGrade As String = "B"
+        Select Case finalGrade
+            Case "A"
+                Console.WriteLine("4.0 GPA")
+            Case "B"
+                Console.WriteLine("3.0 GPA")
+            Case "C"
+                Console.WriteLine("2.0 GPA")
+            Case "D"
+                Console.WriteLine("1.0 GPA")
+            Case Else
+                Console.WriteLine("0.0 GPA")
+        End Select
     End Sub
 End Module
