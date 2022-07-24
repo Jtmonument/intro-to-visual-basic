d705d01a7087a6fdad4a165dfe57e0ed7ce9e834
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 23:17:14 -0500

    Add Format example.

--- Program.vb
+++ Program.vb
@@ -2,8 +2,10 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim number As Integer = 5
-        Dim parsed As String = "This watermelon costs " & number.ToString("C")
+        Dim number As String = "5.342"
+        Console.WriteLine(FormatNumber(number, 2))
+        Console.WriteLine(FormatPercent(number))
+        Dim parsed As String = "The gas price is " & FormatCurrency(number, 3)
         Console.WriteLine(parsed)
     End Sub
 End Module
