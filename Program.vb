dddcfd7d7504367c4c4205730f8359b27a3b1c47
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 22:55:13 -0500

    Add variables and division example.

--- Program.vb
+++ Program.vb
@@ -2,7 +2,10 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("What is your name?")
-        Console.WriteLine("Hello " & Console.ReadLine())
+        Dim a As Integer
+        a = 5
+        Dim b As Integer = 3
+        Console.WriteLine(a \ b) ' \ integer division
+        Console.WriteLine(a / b) ' / float division
     End Sub
 End Module
